3a21f0a744d363d6bd3a9c6b52d6e221986cb4db
Reality <ighoafokereality1@gmail.com>
2025-05-22 13:03:19 +0100

    logging

--- script.js
+++ script.js
@@ -44,6 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
             throw new Error(`Error message:${response.status}`);
           } else {
             const status = await responses[0].json();
+            console.log(status);
             console.log(status.isloggedIn);
             if (status.isloggedIn === "loggedIn") {
               // logout.classList.toggle("toggle_login_logout");
