810af9047f1ee92b15469b4097e917e2b1f717c0
Reality <ighoafokereality1@gmail.com>
2025-06-10 01:44:09 +0100

    still on maintainance

--- oauth/signup-login.js
+++ oauth/signup-login.js
@@ -7,8 +7,8 @@ const email = document.getElementById("email");
 const form = document.getElementById("form");
 email.value = clientEmail;
 email.innerText = clientEmail;
-// const ip = "http://127.0.0.1:1998";
-const ip = "https://cryptomarket-server.onrender.com";
+const ip = "http://127.0.0.1:1998";
+// const ip = "https://cryptomarket-server.onrender.com";
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   processAuthentication(email.value, password.value);
